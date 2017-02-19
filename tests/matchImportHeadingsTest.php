98b4c83b9ad2db735a65474f59316f9707ab90d2
Gordon <gordon@gordonmurray.com>
2017-02-19 14:36:56 +0000

    added SynonymMatch test

--- tests/matchImportHeadingsTest.php
+++ tests/matchImportHeadingsTest.php
@@ -6,7 +6,7 @@ use gordonmurray\matchImportHeadings;
 
 class cleanMatchHeadingsTest extends PHPUnit_Framework_TestCase
 {
-    public function testBasicClean()
+    public function testDirectMatch()
     {
         $matchImportHeadings = new matchImportHeadings();
 
@@ -21,6 +21,35 @@ class cleanMatchHeadingsTest extends PHPUnit_Framework_TestCase
         );
 
         $response = $matchImportHeadings->directMatch($knownHeadings, $headings);
+
+        $this->assertEquals($expectedResponse, $response);
+    }
+
+    public function testSynonymMatch()
+    {
+        $matchImportHeadings = new matchImportHeadings();
+
+        $knownHeadings = array('first name','middle name','last name','gender','date of birth','city','post code','country','place of work','job');
+
+        $headings  = array(
+            array('value'=>'first name','match'=>'first name'),
+            array('value'=>'middle name','match'=>''),
+            array('value'=>'last name', 'match'=>'last name'),
+            array('value'=>'org', 'match'=>'')
+        );
+
+        $synonyms = array(
+            'place of work'=>array('organisation','org','orgname'),
+        );
+
+        $expectedResponse = array(
+            array('value'=>'first name','match'=>'first name'),
+            array('value'=>'middle name','match'=>''),
+            array('value'=>'last name', 'match'=>'last name'),
+            array('value'=>'org', 'match'=>'place of work')
+        );
+
+        $response = $matchImportHeadings->synonymMatch($synonyms, $headings);
 
         $this->assertEquals($expectedResponse, $response);
     }
