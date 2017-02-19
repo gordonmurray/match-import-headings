042ddbc10f7e3fcaf6df0bbad68db8ebbbb94fa6
Gordon <gordon@gordonmurray.com>
2017-02-19 17:34:58 +0000

    code clean

--- src/matchImportHeadings.php
+++ src/matchImportHeadings.php
@@ -7,9 +7,9 @@ class matchImportHeadings
     /**
      * Iterate over the values in an array, make matches to known headings
      *
-     * @param  array  $knownHeadings [pre known headings]
-     * @param  array  $synonymsArray [An array of parent words and common synonyms]
-     * @param  array  $headings [the users headings]
+     * @param array $knownHeadings [pre known headings]
+     * @param array $synonymsArray [An array of parent words and common synonyms]
+     * @param array $headings      [the users headings]
      *
      * @return array $matchedHeadings [updated headings]
      */
@@ -30,8 +30,8 @@ class matchImportHeadings
     /**
      * Look for any direct matchs of the headins when compared to known headings
      *
-     * @param  array  $knownHeadings [pre known headings]
-     * @param  array  $headings [the users headings]
+     * @param array $knownHeadings [pre known headings]
+     * @param array $headings      [the users headings]
      *
      * @return array $matchedHeadings [updated headings]
      */
@@ -39,7 +39,7 @@ class matchImportHeadings
     {
         $matchedHeadings = array();
 
-        foreach ($headings as $key =>$heading) {
+        foreach ($headings as $key => $heading) {
             $matchedHeadings[$key]['value']=$heading;
 
             if (in_array($heading, $knownHeadings)) {
@@ -54,14 +54,14 @@ class matchImportHeadings
     /**
      * Look for any matches of the headings when compared to known heading synonyms
      *
-     * @param  array  $synonymsArray [An array of parent words and common synonyms]
-     * @param  array  $headings [the users headings]
+     * @param array $synonymsArray [An array of parent words and common synonyms]
+     * @param array $headings      [the users headings]
      *
      * @return array $headings [updated headings]
      */
     public function synonymMatch($synonymsArray = array(), $headings = array())
     {
-        foreach ($headings as $key =>$heading) {
+        foreach ($headings as $key => $heading) {
             if (isset($heading['match']) && $heading['match']=='') {
                 foreach ($synonymsArray as $parentString => $synonyms) {
                     if (in_array($heading['value'], $synonyms)) {
@@ -77,14 +77,14 @@ class matchImportHeadings
     /**
      * Try to make a match using the levenshtein function
      *
-     * @param  array  $knownHeadings [pre known headings]
-     * @param  array  $headings [the users headings]
+     * @param array $knownHeadings [pre known headings]
+     * @param array $headings      [the users headings]
      *
      * @return array $headings [updated headings]
      */
     public function matchLevenshtein($knownHeadings = array(), $headings = array())
     {
-        foreach ($headings as $key =>$heading) {
+        foreach ($headings as $key => $heading) {
             if (isset($heading['match']) && $heading['match']=='') {
                 $input = $heading['value'];
 
@@ -93,13 +93,11 @@ class matchImportHeadings
 
                 // loop through words to find the closest
                 foreach ($knownHeadings as $word) {
-
                     // calculate the distance between the input word and the current word
                     $lev = levenshtein($input, $word);
 
                     // check for an exact match
                     if ($lev == 0) {
-
                         // closest word is this one (exact match)
                         $closest = $word;
                         $shortest = 0;
